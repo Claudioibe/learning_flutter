08ace65327846bd89fd57bbeacbd912918046761
Claudio <mac@macs-MacBook-Pro.local>
2023-09-04 18:13:06 +0100

    learnt custom widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,33 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(colors: colors),
-          ),
-          child: const Center(
-            child: Text('Claudio is here'),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+ @override
+ Widget build(context) {
+  return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(251, 35, 6, 92),
+                Color.fromARGB(96, 105, 12, 122)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Claudio is here',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        );
+ }  
+
 }
